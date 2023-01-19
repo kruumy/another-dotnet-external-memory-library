6556be2bfa24b7a08c4371749d351cc98c777a2b
kruumy <joshuampauls@gmail.com>
2023-01-19 11:24:35 -0500

    removed useless method

--- AnotherExternalMemoryLibrary/Extensions/ProcessModuleExtensions.cs
+++ AnotherExternalMemoryLibrary/Extensions/ProcessModuleExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 namespace AnotherExternalMemoryLibrary.Extensions
 {
@@ -10,13 +8,13 @@ namespace AnotherExternalMemoryLibrary.Extensions
         public static ProcessModule GetByName(this ProcessModuleCollection modules, string name)
         {
             foreach (ProcessModule item in modules)
+            {
                 if (item.ModuleName == name)
+                {
                     return item;
-            throw new Exception("Name Did Not Match Any ModuleNames");
-        }
-        public static IEnumerable<ProcessModule> GetEnumerable(this ProcessModuleCollection modules)
-        {
-            return modules.Cast<ProcessModule>();
+                }
+            }
+            throw new ArgumentException(name, nameof(name));
         }
     }
 }
